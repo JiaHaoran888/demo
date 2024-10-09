64beb58fb1cbe05a6728b433ab66defbbaf3b64e
JiaHaoran888 <h.jia.12@student.scu.edu.au>
2024-10-09 20:04:17 +0800

    commit
    commit

---
+++ course.py
@@ -0,0 +1,12 @@
+# 课程页面
+@app.route('/courses/<course_code>/grades/<population_code>')
+def view_course_grades(course_code, population_code):
+    # Logic to handle the request
+    pass
+
+
+import traceback
+
+
+
+
